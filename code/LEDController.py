2747177a3b56c8638f609c4064bfa15f1d2c74e1
Grant K <grantkveton@msn.com>
2019-10-30 13:14:36 -0400

    created a Main.py file, and a new LEDController class (Untested)

---
+++ code/LEDController.py
@@ -0,0 +1,70 @@
+import sys, time
+import RPi.GPIO as GPIO
+
+class LEDController:
+    def __init__(self, redpin, greenpin, bluepin):
+        self.redPin = redpin
+        self.greenPin = greenpin
+        self.bluePin = bluepin
+
+    def blink(self, pin):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.HIGH)
+
+    def turnOff(self, pin):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.LOW)
+
+    def redOn(self):
+        blink(redPin)
+
+    def greenOn(self):
+        blink(greenPin)
+
+    def blueOn(self):
+        blink(bluePin)
+
+    def yellowOn(self):
+        blink(redPin)
+        blink(greenPin)
+
+    def cyanOn(self):
+        blink(greenPin)
+        blink(bluePin)
+
+    def magentaOn(self):
+        blink(redPin)
+        blink(bluePin)
+
+    def whiteOn(self):
+        blink(redPin)
+        blink(greenPin)
+        blink(bluePin)
+
+    def redOff(self):
+        turnOff(redPin)
+
+    def greenOff(self):
+        turnOff(greenPin)
+
+    def blueOff(self):
+        turnOff(bluePin)
+
+    def yellowOff(self):
+        turnOff(redPin)
+        turnOff(greenPin)
+
+    def cyanOff(self):
+        turnOff(greenPin)
+        turnOff(bluePin)
+
+    def magentaOff(self):
+        turnOff(redPin)
+        turnOff(bluePin)
+
+    def whiteOff(self):
+        turnOff(redPin)
+        turnOff(greenPin)
+        turnOff(bluePin)
---
+++ code/Main.py
@@ -0,0 +1,34 @@
+import board
+import busio
+import adafruit_ads1x15.ads1015 as ADS
+from adafruit_ads1x15.analog_in import AnalogIn
+import sys, time
+import RPi.GPIO as GPIO
+import LEDController.py
+
+#CONFIGURE ADS CONVERTER WITH RASPBERRY PI
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1015(i2c)
+#single ended analog input mode
+tempChannel = AnalogIn(ads, ADS.P0)
+IRChannel = AnalogIn(ads, ADS.P1)
+
+def getTemp():
+	temp = 0
+	for i in range(5):
+		temp += (tempChannel.voltage - .5) /.01
+	return temp
+
+def getIR():
+	IRValue = 0
+	for i in range(5):
+		IRValue += IRChannel.value
+	print(IRValue)
+	return
+
+def main():
+    led1 = LEDController(11,13,15)
+    led1.redOn()
+
+while(True):
+    main()

e8fc0aaaa76b1f9c25dabd1667e97e9c65e3d7b3
Grant K <grantkveton@msn.com>
2019-10-30 13:41:46 -0400

    added full LED class into Main

--- code/Main.py
+++ code/Main.py
@@ -4,15 +4,17 @@ import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 import sys, time
 import RPi.GPIO as GPIO
-from Classes.LEDController import LEDController
-
 
 #CONFIGURE ADS CONVERTER WITH RASPBERRY PI
-#i2c = busio.I2C(board.SCL, board.SDA)
-#ads = ADS.ADS1015(i2c)
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1015(i2c)
 #single ended analog input mode
-#tempChannel = AnalogIn(ads, ADS.P0)
-#IRChannel = AnalogIn(ads, ADS.P1)
+tempChannel = AnalogIn(ads, ADS.P0)
+IRChannel = AnalogIn(ads, ADS.P1)
+
+def main():
+    led1 = LEDController(11,13,15)
+    led1.redOn()
 
 def getTemp():
 	temp = 0
@@ -27,9 +29,73 @@ def getIR():
 	print(IRValue)
 	return
 
-def main():
-    led1 = LEDController(11,13,15)
-    led1.redOn()
+class LEDController:
+    def __init__(self, redpin, greenpin, bluepin):
+        self.redPin = redpin
+        self.greenPin = greenpin
+        self.bluePin = bluepin
+
+    def blink(self, pin):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.HIGH)
+
+    def turnOff(self, pin):
+        GPIO.setmode(GPIO.BOARD)
+        GPIO.setup(pin, GPIO.OUT)
+        GPIO.output(pin, GPIO.LOW)
+
+    def redOn(self):
+        self.blink(self.redPin)
+
+    def greenOn(self):
+        self.blink(greenPin)
+
+    def blueOn(self):
+        self.blink(bluePin)
+
+    def yellowOn(self):
+        self.blink(redPin)
+        self.blink(greenPin)
+
+    def cyanOn(self):
+        blink(greenPin)
+        blink(bluePin)
+
+    def magentaOn(self):
+        blink(redPin)
+        blink(bluePin)
+
+    def whiteOn(self):
+        blink(redPin)
+        blink(greenPin)
+        blink(bluePin)
+
+    def redOff(self):
+        turnOff(redPin)
+
+    def greenOff(self):
+        turnOff(greenPin)
+
+    def blueOff(self):
+        turnOff(bluePin)
+
+    def yellowOff(self):
+        turnOff(redPin)
+        turnOff(greenPin)
+
+    def cyanOff(self):
+        turnOff(greenPin)
+        turnOff(bluePin)
+
+    def magentaOff(self):
+        turnOff(redPin)
+        turnOff(bluePin)
+
+    def whiteOff(self):
+        turnOff(redPin)
+        turnOff(greenPin)
+        turnOff(bluePin)
 
 while(True):
     main()

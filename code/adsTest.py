2be316738a2e645fcd6743dd3a630b3be2ab569e
Grant K <grantkveton@msn.com>
2019-11-06 14:21:40 -0500

    password sequence added (untested)

--- code/adsTest.py
+++ code/adsTest.py
@@ -10,6 +10,10 @@ ads = ADS.ADS1015(i2c)
 chan1 = AnalogIn(ads, ADS.P0)
 chan2 = AnalogIn(ads, ADS.P1)
 
+passcodeEntered = False
+passcodeSequence = 0
+passcode = '101'
+
 def getTemp():
 	temp = 0
 	for i in range(5):
@@ -24,13 +28,27 @@ def getIR():
 	return IRValue/5
 
 while True:
+	motionInput = ''
 	if getIR() < 5000.0:
 		print("No motion " + str(getIR()))
+		motionInput = ''
 
 	if getIR() > 20000.0:
 		print("1: Close Motion " + str(getIR()))
+		motionInput = '1'
 
 	if 10000.0 < getIR() < 20000.0:
 		print("0: Far motion " + str(getIR()))
+		motionInput = '0'
 
-	time.sleep(0.2)
+	if motionInput == passcode[passcodeSequence] and motionInput != '' and passcodeEntered == False:
+		if len(passcode)-1 == passcodeSequence:
+			passcodeEntered = True
+			passcodeSequence = 0
+		else:
+			passcodeSequence += 1
+
+	if passcodeEntered:
+		print("Password entered correctly.")
+
+	time.sleep(0.5)

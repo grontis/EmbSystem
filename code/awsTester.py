85bb9c31679b558d29f3f867428ac6e0d4d46f53
Grant K <grantkveton@msn.com>
2019-11-13 15:21:40 -0500

    bug fix

--- code/awsTester.py
+++ code/awsTester.py
@@ -9,6 +9,12 @@ import busio
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 
+i2c = busio.I2C(board.SCL, board.SDA)
+ads = ADS.ADS1015(i2c)
+#single ended analog input mode
+chan1 = AnalogIn(ads, ADS.P0)
+chan2 = AnalogIn(ads, ADS.P1)
+
 
 # Shadow JSON schema:
 #

e764c414aaa3e10dcfec4ae5eb8db2c6289f8d0e
Grant K <grantkveton@msn.com>
2019-11-20 14:18:05 -0500

    aws paths hardcoded, no args

--- code/awsTester.py
+++ code/awsTester.py
@@ -54,25 +54,19 @@ def customShadowCallback_Delete(payload, responseStatus, token):
 
 # Read in command-line parameters
 parser = argparse.ArgumentParser()
-parser.add_argument("-e", "--endpoint", action="store", required=True, dest="host", help="Your AWS IoT custom endpoint")
-parser.add_argument("-r", "--rootCA", action="store", required=True, dest="rootCAPath", help="Root CA file path")
-parser.add_argument("-c", "--cert", action="store", dest="certificatePath", help="Certificate file path")
-parser.add_argument("-k", "--key", action="store", dest="privateKeyPath", help="Private key file path")
 parser.add_argument("-p", "--port", action="store", dest="port", type=int, help="Port number override")
 parser.add_argument("-w", "--websocket", action="store_true", dest="useWebsocket", default=False,
                     help="Use MQTT over WebSocket")
-parser.add_argument("-n", "--thingName", action="store", dest="thingName", default="Bot", help="Targeted thing name")
-parser.add_argument("-id", "--clientId", action="store", dest="clientId", default="basicShadowUpdater", help="Targeted client id")
 
 args = parser.parse_args()
-host = args.host
-rootCAPath = args.rootCAPath
-certificatePath = args.certificatePath
-privateKeyPath = args.privateKeyPath
+host = "a37zzqz465wad2-ats.iot.us-east-1.amazonaws.com"
+rootCAPath = "~/EmbSystem/keys/AmazonRootCA1.pem"
+certificatePath = "~/EmbSystem/keys/pi-certificate.pem.crt.txt"
+privateKeyPath = "~/EmbSystem/keys/pi-private.pem.key"
 port = args.port
-useWebsocket = args.useWebsocket
-thingName = args.thingName
-clientId = args.clientId
+useWebsocket = False
+thingName = "RaspberryPi"
+clientId = "RaspberryPi"
 
 if args.useWebsocket and args.certificatePath and args.privateKeyPath:
     parser.error("X.509 cert authentication and WebSocket are mutual exclusive. Please pick one.")
